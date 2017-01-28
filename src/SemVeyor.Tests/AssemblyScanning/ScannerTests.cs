c7d0061feed225ec007a2ecfacc57bc34b244917
andy <andy@andydote.co.uk>
2017-01-28 18:13:26 +0200

    assemblyScanning: stub structure

---
+++ src/SemVeyor.Tests/AssemblyScanning/ScannerTests.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+using SemVeyor.AssemblyScanning;
+using Shouldly;
+using StandardLibrary;
+using Xunit;
+
+namespace SemVeyor.Tests.AssemblyScanning
+{
+	public class ScannerTests
+	{
+		[Fact]
+		public void FactMethodName()
+		{
+			var asm = typeof(PublicEmptyClass).Assembly;
+
+			var scanner = new Scanner();
+			var results = scanner.Scan(asm);
+
+			results.Select(tc => tc.FullName).ShouldBe(new []
+			{
+				typeof(PublicEmptyClass).FullName,
+				typeof(PublicStaticClass).FullName
+			});
+		}
+	}
+}
---
+++ src/SemVeyor/AssemblyScanning/Scanner.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace SemVeyor.AssemblyScanning
+{
+	public class Scanner
+	{
+		public IEnumerable<TypeContent> Scan(Assembly assembly)
+		{
+			throw new NotImplementedException();
+		}
+	}
+
+	public class TypeContent
+	{
+		public IEnumerable<string> Properties { get; set; }
+		public IEnumerable<string> Methods { get; set; }
+		public IEnumerable<string> Fields { get; set; }
+		public IEnumerable<string> Constructors { get; set; }
+
+		public string Name { get; set; }
+		public string Namespace { get; set; }
+		public string FullName => $"{Namespace}.{Name}";
+	}
+}

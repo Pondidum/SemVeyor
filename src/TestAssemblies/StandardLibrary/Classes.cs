1c5513687f304d07fb5cdfc3cff74a46f9a5a95a
andy <andy@andydote.co.uk>
2017-01-28 18:11:59 +0200

    tests: add test assemblies projects

---
+++ src/TestAssemblies/StandardLibrary/Classes.cs
@@ -0,0 +1,20 @@
+﻿namespace StandardLibrary
+{
+	public class PublicEmptyClass
+	{
+	}
+
+	public static class PublicStaticClass
+	{
+	}
+
+	internal class InternalEmptyClass
+	{
+
+	}
+
+	internal static class InternalStaticClass
+	{
+
+	}
+}

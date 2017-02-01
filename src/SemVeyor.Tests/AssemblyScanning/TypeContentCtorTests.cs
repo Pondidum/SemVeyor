06d95ff8c1a9611819067ee233831b12a8205ac7
andy <andy@andydote.co.uk>
2017-02-01 20:35:18 +0200

    tests: move test class to separate file

--- src/SemVeyor.Tests/AssemblyScanning/TypeContentCtorTests.cs
+++ src/SemVeyor.Tests/AssemblyScanning/TypeContentCtorTests.cs
@@ -31,30 +31,4 @@ namespace SemVeyor.Tests.AssemblyScanning
 			private TestType(Guid arg) {}
 		}
 	}
-
-	public class TypeContentFieldTests : TypeContentTest<TypeContentFieldTests.TestType>
-	{
-		[Fact]
-		public void There_are_2_fields() => Content.Fields.Count().ShouldBe(2);
-
-		[Fact]
-		public void The_public_field_is_listed() => Content.Fields.ShouldContain(x => x.Visibility == Visibility.Public);
-
-		[Fact]
-		public void The_protected_field_is_listed() => Content.Fields.ShouldContain(x => x.Visibility == Visibility.Protected);
-
-		[Fact]
-		public void The_internal_field_is_not_listed() => Content.Fields.ShouldNotContain(x => x.Visibility == Visibility.Internal);
-
-		[Fact]
-		public void The_private_field_is_not_listed() => Content.Fields.ShouldNotContain(x => x.Visibility == Visibility.Private);
-
-		public class TestType
-		{
-			public int PublicField;
-			internal int InternalField;
-			protected int ProtectedField;
-			private int PrivateField;
-		}
-	}
 }

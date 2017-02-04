07e9bab8b71dfe805af21b58a9ee816b4a1690e5
andy <andy@andydote.co.uk>
2017-02-04 22:50:33 +0200

    assemblyScanning: add genericArgumentDetails

---
+++ src/SemVeyor.Tests/AssemblyScanning/GenericArgumentDetailsTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using SemVeyor.AssemblyScanning;
+using Shouldly;
+using Xunit;
+
+namespace SemVeyor.Tests.AssemblyScanning
+{
+	public class GenericArgumentDetailsTests
+	{
+		private readonly GenericArgumentDetails _details;
+
+		public GenericArgumentDetailsTests()
+		{
+			var generic = typeof(GenericArgumentDetailsTests)
+				.GetMethod(nameof(GenericMethod))
+				.GetGenericArguments()
+				.Last();
+
+			_details = GenericArgumentDetails.From(generic);
+		}
+
+		[Fact]
+		public void The_name_is_popualted() => _details.Name.ShouldBe("TValue");
+
+		[Fact]
+		public void The_position_is_populated() => _details.Positon.ShouldBe(1);
+
+		[Fact]
+		public void The_constraints_are_populated() => _details.Constraints.ShouldBe(new[] { nameof(IEnumerable) });
+
+		public TValue GenericMethod<TKey, TValue>(TValue test, Action<TValue> action)
+			where TValue : IEnumerable
+		{
+			throw new NotSupportedException();
+		}
+	}
+}
---
+++ src/SemVeyor/AssemblyScanning/GenericArgumentDetails.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SemVeyor.AssemblyScanning
+{
+	public class GenericArgumentDetails
+	{
+		public int Positon { get; set; }
+		public string Name { get; set; }
+		public IEnumerable<string> Constraints { get; set; }
+
+		public static GenericArgumentDetails From(Type type)
+		{
+			return new GenericArgumentDetails
+			{
+				Name = type.Name,
+				Positon = type.GenericParameterPosition,
+				Constraints =  type.GetGenericParameterConstraints().Select(c => c.Name)
+			};
+		}
+	}
+}

2550e426053f7580bf361c926d846c707edd07bd
andy <andy@andydote.co.uk>
2017-02-11 18:21:55 +0200

    visibility: add tests to cover type visiblity

---
+++ src/SemVeyor.Tests/AssemblyScanning/VisibilityTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using SemVeyor.AssemblyScanning;
+using Shouldly;
+using Xunit;
+
+namespace SemVeyor.Tests.AssemblyScanning
+{
+	public class VisibilityTests
+	{
+		[Theory]
+		[InlineData(typeof(NestedPublicClass), Visibility.Public)]
+		[InlineData(typeof(NestedInternalClass), Visibility.Internal)]
+		[InlineData(typeof(NestedProtectedClass), Visibility.Protected)]
+		[InlineData(typeof(NestedPrivateClass), Visibility.Private)]
+		[InlineData(typeof(StandalonePublicClass), Visibility.Public)]
+		[InlineData(typeof(StandaloneInternalClass), Visibility.Internal)]
+		public void When_checking_a_class(Type type, Visibility expected) => type.GetVisibility().ShouldBe(expected);
+
+		public class NestedPublicClass { }
+		internal class NestedInternalClass { }
+		protected class NestedProtectedClass { }
+		private class NestedPrivateClass { }
+	}
+
+	public class StandalonePublicClass { }
+	internal class StandaloneInternalClass { }
+}

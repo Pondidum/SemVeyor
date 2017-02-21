51759e4e2b8ec5b961b5e24a8b5a994f73820b42
andy <andy@andydote.co.uk>
2017-02-21 18:28:02 +0200

    typeDetailsTests: refactor tests

--- src/SemVeyor.Tests/AssemblyScanning/TypeDetailsTests.cs
+++ src/SemVeyor.Tests/AssemblyScanning/TypeDetailsTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using SemVeyor.AssemblyScanning;
 using SemVeyor.AssemblyScanning.Events;
 using SemVeyor.Tests.Builder;
@@ -9,212 +10,163 @@ namespace SemVeyor.Tests.AssemblyScanning
 {
 	public class TypeDetailsTests
 	{
+		private static void ChangesShouldBe(TypeDetails older, TypeDetails newer, params Type[] expected)
+		{
+			older
+				.UpdatedTo(newer)
+				.Select(c => c.GetType())
+				.ShouldBe(expected);
+		}
+
 		[Fact]
 		public void When_checking_the_log()
 		{
-			var first = Build.Type("first")
+			var older = Build.Type("first")
 				.WithField(Build.Field<int>("_first"))
 				.Build();
 
-			var second = Build.Type("first")
+			var newer = Build.Type("first")
 				.WithField(Build.Field<int>("_first"))
 				.Build();
 
-			var change = first.UpdatedTo(second);
-
-			change.ShouldBeEmpty();
+			ChangesShouldBe(older, newer);
 		}
 
 		[Fact]
 		public void When_a_field_has_been_removed()
 		{
-			var first = Build.Type("first")
+			var older = Build.Type("first")
 				.WithField(Build.Field<int>("_first"))
 				.Build();
 
-			var second = Build.Type("first")
+			var newer = Build.Type("first")
 				.Build();
 
-			var change = first.UpdatedTo(second);
-
-			change.Select(e => e.GetType()).ShouldBe(new[]
-			{
-				typeof(FieldRemoved)
-			});
+			ChangesShouldBe(older, newer, typeof(FieldRemoved));
 		}
 
 		[Fact]
 		public void When_a_field_has_been_added()
 		{
-			var first = Build.Type("first")
+			var older = Build.Type("first")
 				.WithField(Build.Field<int>("_first"))
 				.Build();
 
-			var second = Build.Type("first")
+			var newer = Build.Type("first")
 				.WithField(Build.Field<int>("_first"))
 				.WithField(Build.Field<int>("_second"))
 				.Build();
 
-			var change = first.UpdatedTo(second);
-
-			change.Select(e => e.GetType()).ShouldBe(new[]
-			{
-				typeof(FieldAdded)
-			});
+			ChangesShouldBe(older, newer, typeof(FieldAdded));
 		}
 
 		[Fact]
 		public void When_a_field_has_been_renamed()
 		{
-			var first = Build.Type("first")
+			var older = Build.Type("first")
 				.WithField(Build.Field<int>("_first"))
 				.Build();
 
-			var second = Build.Type("first")
+			var newer = Build.Type("first")
 				.WithField(Build.Field<int>("_second"))
 				.Build();
 
-			var change = first.UpdatedTo(second);
-
-			change.Select(e => e.GetType()).ShouldBe(new[]
-			{
-				typeof(FieldRemoved),
-				typeof(FieldAdded)
-			});
+			ChangesShouldBe(older, newer, typeof(FieldRemoved),
+				typeof(FieldAdded));
 		}
 
 		[Fact]
 		public void When_a_field_has_becomes_less_visible()
 		{
-			var first = Build.Type("first")
+			var older = Build.Type("first")
 				.WithField(Build.Field<int>("_first").WithVisibility(Visibility.Public))
 				.Build();
 
-			var second = Build.Type("first")
+			var newer = Build.Type("first")
 				.WithField(Build.Field<int>("_first"))
 				.Build();
 
-			var change = first.UpdatedTo(second);
-
-			change.Select(e => e.GetType()).ShouldBe(new[]
-			{
-				typeof(FieldVisibilityDecreased),
-			});
+			ChangesShouldBe(older, newer, typeof(FieldVisibilityDecreased));
 		}
 
 		[Fact]
 		public void When_a_field_has_becomes_more_visible()
 		{
-			var first = Build.Type("first")
+			var older = Build.Type("first")
 				.WithField(Build.Field<int>("_first"))
 				.Build();
 
-			var second = Build.Type("first")
+			var newer = Build.Type("first")
 				.WithField(Build.Field<int>("_first").WithVisibility(Visibility.Public))
 				.Build();
 
-			var change = first.UpdatedTo(second);
-
-			change.Select(e => e.GetType()).ShouldBe(new[]
-			{
-				typeof(FieldVisibilityIncreased),
-			});
+			ChangesShouldBe(older, newer, typeof(FieldVisibilityIncreased));
 		}
 
 		[Fact]
 		public void When_a_method_has_been_added()
 		{
-			var first = Build.Type("").Build();
-			var second = Build.Type("").WithMethods(Build.Method("One")).Build();
+			var older = Build.Type("").Build();
+			var newer = Build.Type("").WithMethods(Build.Method("One")).Build();
 
-			var changes = first.UpdatedTo(second);
-
-			changes.Select(c => c.GetType()).ShouldBe(new []
-			{
-				typeof(MethodAdded)
-			});
+			ChangesShouldBe(older, newer, typeof(MethodAdded));
 		}
 
 		[Fact]
 		public void When_a_method_has_been_removed()
 		{
-			var first = Build.Type("").WithMethods(Build.Method("One")).Build();
-			var second = Build.Type("").Build();
+			var older = Build.Type("").WithMethods(Build.Method("One")).Build();
+			var newer = Build.Type("").Build();
 
-			var changes = first.UpdatedTo(second);
-
-			changes.Select(c => c.GetType()).ShouldBe(new []
-			{
-				typeof(MethodRemoved)
-			});
+			ChangesShouldBe(older, newer, typeof(MethodRemoved));
 		}
 
 		[Fact]
 		public void When_a_method_has_become_more_visible()
 		{
-			var first = Build.Type("").WithMethods(Build.Method("One")).Build();
-			var second = Build.Type("").WithMethods(Build.Method("One").WithVisibility(Visibility.Public)).Build();
+			var older = Build.Type("").WithMethods(Build.Method("One")).Build();
+			var newer = Build.Type("").WithMethods(Build.Method("One").WithVisibility(Visibility.Public)).Build();
 
-			var changes = first.UpdatedTo(second);
-
-			changes.Select(c => c.GetType()).ShouldBe(new []
-			{
-				typeof(MethodVisibilityIncreased)
-			});
+			ChangesShouldBe(older, newer, typeof(MethodVisibilityIncreased));
 		}
 
 		[Fact]
 		public void When_a_method_has_become_less_visible()
 		{
-			var first = Build.Type("").WithMethods(Build.Method("One").WithVisibility(Visibility.Public)).Build();
-			var second = Build.Type("").WithMethods(Build.Method("One")).Build();
+			var older = Build.Type("").WithMethods(Build.Method("One").WithVisibility(Visibility.Public)).Build();
+			var newer = Build.Type("").WithMethods(Build.Method("One")).Build();
 
-			var changes = first.UpdatedTo(second);
-
-			changes.Select(c => c.GetType()).ShouldBe(new []
-			{
-				typeof(MethodVisibilityDecreased)
-			});
+			ChangesShouldBe(older, newer, typeof(MethodVisibilityDecreased));
 		}
 
 		[Fact]
 		public void When_a_method_overload_is_added()
 		{
-			var first = Build.Type("")
+			var older = Build.Type("")
 				.WithMethods(Build.Method("One"))
 				.Build();
 
-			var second = Build.Type("")
+			var newer = Build.Type("")
 				.WithMethods(Build.Method("One"))
 				.WithMethods(Build.Method("One").WithArguments(Build.Argument<int>("value")))
 				.Build();
 
-			var changes = first.UpdatedTo(second);
-
-			changes.Select(c => c.GetType()).ShouldBe(new[]
-			{
-				typeof(MethodAdded)
-			});
+			ChangesShouldBe(older, newer, typeof(MethodAdded));
 		}
 
 		[Fact]
 		public void When_a_method_overload_is_removed()
 		{
-			var first = Build.Type("")
+			var older = Build.Type("")
 				.WithMethods(Build.Method("One"))
 				.WithMethods(Build.Method("One").WithArguments(Build.Argument<int>("value")))
 				.Build();
 
-			var second = Build.Type("")
+			var newer = Build.Type("")
 				.WithMethods(Build.Method("One"))
 				.Build();
 
-			var changes = first.UpdatedTo(second);
-
-			changes.Select(c => c.GetType()).ShouldBe(new[]
-			{
-				typeof(MethodRemoved)
-			});
+			ChangesShouldBe(older, newer, typeof(MethodRemoved));
 		}
 
 		[Fact]
@@ -225,10 +177,7 @@ namespace SemVeyor.Tests.AssemblyScanning
 
 			var changes = older.UpdatedTo(newer);
 
-			changes.Select(c => c.GetType()).ShouldBe(new []
-			{
-				typeof(PropertyAdded)
-			});
+			ChangesShouldBe(older, newer, typeof(PropertyAdded));
 		}
 
 		[Fact]
@@ -237,12 +186,7 @@ namespace SemVeyor.Tests.AssemblyScanning
 			var older = Build.Type("").WithProperties(Build.Property<int>("Prop"), Build.Property<string>("Other")).Build();
 			var newer = Build.Type("").WithProperties(Build.Property<int>("Prop")).Build();
 
-			var changes = older.UpdatedTo(newer);
-
-			changes.Select(c => c.GetType()).ShouldBe(new []
-			{
-				typeof(PropertyRemoved)
-			});
+			ChangesShouldBe(older, newer, typeof(PropertyRemoved));
 		}
 
 		[Fact]
@@ -251,13 +195,7 @@ namespace SemVeyor.Tests.AssemblyScanning
 			var older = Build.Type("").WithProperties(Build.Property<int>("Prop")).Build();
 			var newer = Build.Type("").WithProperties(Build.Property<int>("NewProp")).Build();
 
-			var changes = older.UpdatedTo(newer);
-
-			changes.Select(c => c.GetType()).ShouldBe(new []
-			{
-				typeof(PropertyRemoved),
-				typeof(PropertyAdded)
-			});
+			ChangesShouldBe(older, newer, typeof(PropertyRemoved), typeof(PropertyAdded));
 		}
 
 		[Fact]
@@ -266,12 +204,7 @@ namespace SemVeyor.Tests.AssemblyScanning
 			var older = Build.Type("").WithProperties(Build.Property<int>("Prop")).Build();
 			var newer = Build.Type("").WithProperties(Build.Property<int>("Prop"), Build.Property<string>("PropTwo")).Build();
 
-			var changes = older.UpdatedTo(newer);
-
-			changes.Select(c => c.GetType()).ShouldBe(new []
-			{
-				typeof(PropertyAdded)
-			});
+			ChangesShouldBe(older, newer, typeof(PropertyAdded));
 		}
 
 		[Fact]
@@ -280,12 +213,7 @@ namespace SemVeyor.Tests.AssemblyScanning
 			var older = Build.Type("").WithProperties(Build.Property<int>("Prop"), Build.Property<string>("PropTwo")).Build();
 			var newer = Build.Type("").WithProperties(Build.Property<int>("Prop")).Build();
 
-			var changes = older.UpdatedTo(newer);
-
-			changes.Select(c => c.GetType()).ShouldBe(new []
-			{
-				typeof(PropertyRemoved)
-			});
+			ChangesShouldBe(older, newer, typeof(PropertyRemoved));
 		}
 	}
 }

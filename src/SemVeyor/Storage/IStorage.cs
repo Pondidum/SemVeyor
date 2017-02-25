9af982a5d4019fa1d2089e01642c5e98bcc202af
andy <andy@andydote.co.uk>
2017-02-25 20:57:01 +0200

    storage: basic storage interface

---
+++ src/SemVeyor/Storage/IStorage.cs
@@ -0,0 +1,50 @@
+﻿using System.IO;
+using System.Linq;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Serialization;
+using SemVeyor.Domain;
+
+namespace SemVeyor.Storage
+{
+	public interface IStorage
+	{
+		void Write(AssemblyDetails details);
+		AssemblyDetails Read();
+	}
+
+	public class FileStore : IStorage
+	{
+		private static readonly JsonSerializerSettings Settings = new JsonSerializerSettings
+		{
+			ContractResolver = new CamelCasePropertyNamesContractResolver(),
+			Converters =
+			{
+				new StringEnumConverter()
+
+			},
+			Formatting = Formatting.None
+		};
+
+		private readonly string _path;
+
+		public FileStore(string path)
+		{
+			_path = path;
+		}
+
+		public void Write(AssemblyDetails details)
+		{
+			var json = JsonConvert.SerializeObject(details, Settings);
+
+			File.AppendAllLines(_path, new [] { json });
+		}
+
+		public AssemblyDetails Read()
+		{
+			var json = File.ReadAllLines(_path).Last();
+
+			return JsonConvert.DeserializeObject<AssemblyDetails>(json);
+		}
+	}
+}

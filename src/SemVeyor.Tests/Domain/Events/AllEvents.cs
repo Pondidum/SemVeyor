4313043f0d71bbf031047dfa9ba1a91f5c6da7c8
andy <andy@andydote.co.uk>
2017-02-26 11:22:35 +0200

    tests: check all events implement either IMajor or IMinor

---
+++ src/SemVeyor.Tests/Domain/Events/AllEvents.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using SemVeyor.Domain.Events;
+using Shouldly;
+using Xunit;
+
+namespace SemVeyor.Tests.Domain.Events
+{
+	public class AllEvents
+	{
+		private static readonly Type EventBase = typeof(IMajor);
+		public static IEnumerable<object[]> Events => EventBase
+			.Assembly
+			.GetExportedTypes()
+			.Where(t => t.Namespace == EventBase.Namespace)
+			.Where(t => t.IsClass && t.IsAbstract == false)
+			.Select(t => new object[] { t });
+
+		[Theory]
+		[MemberData("Events")]
+		public void All_events_implement_either_IMajor_or_IMinor(Type eventType)
+		{
+			eventType.GetInterfaces().ShouldContain(t => t == typeof(IMajor) || t == typeof(IMinor));
+		}
+	}
+}

2a8124cf3ede6d810fb542e3cfb03af861f3ec8e
andy <andy@andydote.co.uk>
2017-03-05 15:03:04 +0200

    getAllPropertiesQueryTests: implement

--- src/SemVeyor.Tests/Domain/TypeDetailsCtorTests.cs
+++ src/SemVeyor.Tests/Domain/TypeDetailsCtorTests.cs
@@ -33,52 +33,8 @@ namespace SemVeyor.Tests.Domain
 		[Fact]
 		public void There_are_2_fields() => Details.Fields.Count().ShouldBe(2);
 
-
-
 		[Fact]
 		public void There_are_7_properties() => Details.Properties.Count().ShouldBe(7);
-
-		[Fact]
-		public void The_public_property_is_listed() => Details.Properties.ShouldContain( x => x.Name == nameof(TestType.Property));
-
-		[Fact]
-		public void The_readonly_property_is_listed() => Details.Properties.ShouldContain(x => x.Name == nameof(TestType.ReadonlyProperty));
-
-		[Fact]
-		public void The_private_setter_property_is_listed() => Details.Properties.ShouldContain(x => x.Name == nameof(TestType.PrivateSetProperty));
-
-		[Fact]
-		public void The_internal_setter_property_is_listed() => Details.Properties.ShouldContain(x => x.Name == nameof(TestType.InternalSetProperty));
-
-		[Fact]
-		public void The_protected_setter_property_is_listed() => Details.Properties.ShouldContain(x => x.Name == nameof(TestType.ProtectedSetProperty));
-
-		[Fact]
-		public void The_protected_property_is_listed() => Details.Properties.ShouldContain(x => x.Name == "ProtectedProperty");
-
-		[Fact]
-		public void The_internal_property_is_not_listed() => Details.Properties.ShouldNotContain(x => x.Name == "InternalProperty");
-
-		[Fact]
-		public void The_private_property_is_not_listed() => Details.Properties.ShouldNotContain(x => x.Name == "PrivateProperty");
-
-		[Fact]
-		public void The_property_name_is_populated() => Property.Name.ShouldBe(nameof(TestType.InternalSetProperty));
-
-		[Fact]
-		public void The_property_type_is_populated() => Property.Type.ShouldBe(typeof(int));
-
-		[Fact]
-		public void The_property_setter_visibility_is_populated() => Property.SetterVisibility.ShouldBe(Visibility.Internal);
-
-		[Fact]
-		public void The_propety_with_no_setters_visibility_is_null() => ReadOnlyProperty.SetterVisibility.ShouldBeNull();
-
-		[Fact]
-		public void An_indexed_property_argument_is_populated() => IndexedProperty.Parameters.Count().ShouldBe(1);
-
-		[Fact]
-		public void An_indexed_property_name_is_populated() => IndexedProperty.Name.ShouldBe("Item");
 
 
 
@@ -123,11 +79,8 @@ namespace SemVeyor.Tests.Domain
 
 
 
-		
+
 		private MethodDetails PublicMethod => Details.Methods.ByVisibility(Visibility.Public);
-		private PropertyDetails Property => Details.Properties.FirstOrDefault(p => p.Name == nameof(TestType.InternalSetProperty));
-		private PropertyDetails ReadOnlyProperty => Details.Properties.FirstOrDefault(p => p.Name == nameof(TestType.ReadonlyProperty));
-		private PropertyDetails IndexedProperty => Details.Properties.FirstOrDefault(p => p.Type== typeof(string));
 		private MethodDetails GenericMethod => Details.Methods.Single(m => m.Name == TestType.GenericMethodName);
 
 		private static int MethodsOnObject() => typeof(object)

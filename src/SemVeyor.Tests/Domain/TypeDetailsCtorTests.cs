04999fa3c3cd5be4e6d29e4d5478009d64026ec1
andy <andy@andydote.co.uk>
2017-03-05 14:41:11 +0200

    tests: extract the testType to be usable by all tests

--- src/SemVeyor.Tests/Domain/TypeDetailsCtorTests.cs
+++ src/SemVeyor.Tests/Domain/TypeDetailsCtorTests.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using System.Reflection;
 using SemVeyor.Domain;
+using SemVeyor.Tests.TestUtils;
 using Shouldly;
 using Xunit;
 
@@ -170,49 +171,5 @@ namespace SemVeyor.Tests.Domain
 		private static int MethodsOnObject() => typeof(object)
 			.GetMethods(BindingFlags.Instance | BindingFlags.Public | BindingFlags.DeclaredOnly)
 			.Count();
-
-		public class TestType: ParentType, ITestInterfaceOne, ITestInterfaceTwo
-		{
-			public int PublicField;
-			internal int InternalField;
-			protected int ProtectedField;
-			private int PrivateField;
-
-
-			public TestType() {}
-			internal TestType(int arg) {}
-			protected TestType(string arg) {}
-			private TestType(Guid arg) {}
-
-			public int Property { get; set; }
-			public int ReadonlyProperty { get; }
-
-			public int PrivateSetProperty { get; private set; }
-			public int InternalSetProperty { get; internal set; }
-			public int ProtectedSetProperty { get; protected set; }
-
-			internal int InternalProperty { get; set; }
-			protected int ProtectedProperty { get; set; }
-			private int PrivateProperty { get; set; }
-
-			public string this[int index]
-			{
-				get { throw new Exception(); }
-				set { throw new Exception(); }
-			}
-
-			public int Method(string first, int second, Action<int> third) { throw new NotSupportedException(); }
-			internal int InternalMethod() { throw new NotSupportedException(); }
-			protected int ProtectedMethod() { throw new NotSupportedException(); }
-			private int PrivateMethod() { throw new NotSupportedException(); }
-
-			protected T GenericMethod<T>(T test, Action<T> action) { throw new NotSupportedException(); }
-
-			public static string GenericMethodName => nameof(GenericMethod);
-		}
-
-		public class ParentType {}
-		public interface ITestInterfaceOne {}
-		public interface ITestInterfaceTwo {}
 	}
 }
---
+++ src/SemVeyor.Tests/TestUtils/TestType.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace SemVeyor.Tests.TestUtils
+{
+	public class TestType: ParentType, ITestInterfaceOne, ITestInterfaceTwo
+	{
+		public int PublicField;
+		internal int InternalField;
+		protected int ProtectedField;
+		private int PrivateField;
+
+
+		public TestType() {}
+		internal TestType(int arg) {}
+		protected TestType(string arg) {}
+		private TestType(Guid arg) {}
+
+
+		public int Property { get; set; }
+		public int ReadonlyProperty { get; }
+
+		public int PrivateSetProperty { get; private set; }
+		public int InternalSetProperty { get; internal set; }
+		public int ProtectedSetProperty { get; protected set; }
+
+		internal int InternalProperty { get; set; }
+		protected int ProtectedProperty { get; set; }
+		private int PrivateProperty { get; set; }
+
+		public string this[int index]
+		{
+			get { throw new Exception(); }
+			set { throw new Exception(); }
+		}
+
+		public int Method(string first, int second, Action<int> third) { throw new NotSupportedException(); }
+		internal int InternalMethod() { throw new NotSupportedException(); }
+		protected int ProtectedMethod() { throw new NotSupportedException(); }
+		private int PrivateMethod() { throw new NotSupportedException(); }
+
+		protected T GenericMethod<T>(T test, Action<T> action) { throw new NotSupportedException(); }
+
+		public static string GenericMethodName => nameof(GenericMethod);
+	}
+
+	public class ParentType {}
+	public interface ITestInterfaceOne {}
+	public interface ITestInterfaceTwo {}
+}

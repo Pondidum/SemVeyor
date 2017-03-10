e9d7ec49cba5a34548d01d8c78cb487c3fc20d7d
andy <andy@andydote.co.uk>
2017-03-10 19:05:16 +0200

    extensions: add dictionary<>.getOrDefault

---
+++ src/SemVeyor.Tests/Infrastructure/ExtensionsTests.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using SemVeyor.Infrastructure;
+using Shouldly;
+using Xunit;
+
+namespace SemVeyor.Tests.Infrastructure
+{
+	public class ExtensionsTests
+	{
+		[Theory]
+		[InlineData("good key", 123)]
+		[InlineData("bad key", 456)]
+		public void GetOrDefault_returns_correct_value(string key, int expected)
+		{
+			var dict = new Dictionary<string, int> { { "good key", 123 } };
+
+			dict.GetOrDefault(key, 456).ShouldBe(expected);
+		}
+	}
+}
---
+++ src/SemVeyor/Infrastructure/Extensions.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace SemVeyor.Infrastructure
+{
+	public static class Extensions
+	{
+		public static TValue GetOrDefault<TKey, TValue>(this IDictionary<TKey, TValue> self, TKey key, TValue defaultValue)
+		{
+			TValue value;
+
+			return self.TryGetValue(key, out value)
+				? value
+				: defaultValue;
+		}
+	}
+}

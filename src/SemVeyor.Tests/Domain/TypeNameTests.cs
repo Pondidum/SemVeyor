7a8b2a223e64c94b44d9a063fda8d0a337b58775
andy <andy@andydote.co.uk>
2017-05-16 18:58:22 +0300

    domain: add typeName type

---
+++ src/SemVeyor.Tests/Domain/TypeNameTests.cs
@@ -0,0 +1,75 @@
+﻿using SemVeyor.Domain;
+using Shouldly;
+using Xunit;
+
+namespace SemVeyor.Tests.Domain
+{
+	public class TypeNameTests
+	{
+		[Fact]
+		public void When_comparing_the_same_instance()
+		{
+			var name = new TypeName("wat");
+
+			ShouldMatch(name, name);
+		}
+
+		[Fact]
+		public void When_comparing_different_matching_instances()
+		{
+			var a = new TypeName("wat is this");
+			var b = new TypeName("wat is this");
+
+			ShouldMatch(a, b);
+		}
+
+		[Fact]
+		public void When_comparing_different_non_matching_instances()
+		{
+			var a = new TypeName("wat");
+			var b = new TypeName("is this?");
+
+			ShouldNotMatch(a, b);
+		}
+
+		private void ShouldMatch(TypeName a, TypeName b)
+		{
+			typeof(TypeName).ShouldSatisfyAllConditions(
+				() => a.Equals(b).ShouldBeTrue(),
+				() => b.Equals(a).ShouldBeTrue(),
+
+				() => (a == b).ShouldBeTrue(),
+				() => (b == a).ShouldBeTrue(),
+
+				() => (a != b).ShouldBeFalse(),
+				() => (b != a).ShouldBeFalse(),
+
+				() => a.GetHashCode().ShouldBe(b.GetHashCode()),
+				() => b.GetHashCode().ShouldBe(a.GetHashCode()),
+
+				() => object.Equals(a, b).ShouldBeTrue(),
+				() => object.Equals(b, a).ShouldBeTrue()
+			);
+		}
+
+		private void ShouldNotMatch(TypeName a, TypeName b)
+		{
+			typeof(TypeName).ShouldSatisfyAllConditions(
+				() => a.Equals(b).ShouldBeFalse(),
+				() => b.Equals(a).ShouldBeFalse(),
+
+				() => (a == b).ShouldBeFalse(),
+				() => (b == a).ShouldBeFalse(),
+
+				() => (a != b).ShouldBeTrue(),
+				() => (b != a).ShouldBeTrue(),
+
+				() => a.GetHashCode().ShouldNotBe(b.GetHashCode()),
+				() => b.GetHashCode().ShouldNotBe(a.GetHashCode()),
+
+				() => object.Equals(a, b).ShouldBeFalse(),
+				() => object.Equals(b, a).ShouldBeFalse()
+			);
+		}
+	}
+}
---
+++ src/SemVeyor/Domain/TypeName.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace SemVeyor.Domain
+{
+	public struct TypeName : IEquatable<TypeName>
+	{
+		private readonly string _fullname;
+
+		public TypeName(string fullname)
+		{
+			if (string.IsNullOrWhiteSpace(fullname))
+				throw new ArgumentNullException(nameof(fullname));
+
+			_fullname = fullname;
+		}
+
+		public bool Equals(TypeName other)
+		{
+			return string.Equals(_fullname, other._fullname);
+		}
+
+		public override bool Equals(object obj)
+		{
+			if (ReferenceEquals(null, obj)) return false;
+			return obj is TypeName && Equals((TypeName)obj);
+		}
+
+		public override int GetHashCode()
+		{
+			return _fullname.GetHashCode();
+		}
+
+		public static bool operator ==(TypeName left, TypeName right)
+		{
+			return left.Equals(right);
+		}
+
+		public static bool operator !=(TypeName left, TypeName right)
+		{
+			return !left.Equals(right);
+		}
+	}
+}

b2dd261fe5f9b99d1c402559e4e33840d8344c2d
andy <andy@andydote.co.uk>
2017-05-18 11:16:17 +0300

    queries: stub out constructors query

--- src/SemVeyor/Scanning/SourceCode/Queries/GetTypeQuery.cs
+++ src/SemVeyor/Scanning/SourceCode/Queries/GetTypeQuery.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using System.Linq;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
@@ -9,10 +11,12 @@ namespace SemVeyor.Scanning.SourceCode.Queries
 	public class GetTypeQuery
 	{
 		private readonly GetAllFieldsQuery _getFields;
+		private readonly GetAllCtorsQuery _getConstructors;
 
 		public GetTypeQuery()
 		{
 			_getFields = new GetAllFieldsQuery();
+			_getConstructors = new GetAllCtorsQuery();
 		}
 
 		public TypeDetails Execute(SemanticModel model, ClassDeclarationSyntax classDeclaration)
@@ -26,8 +30,17 @@ namespace SemVeyor.Scanning.SourceCode.Queries
 				Visibility = Helpers.VisibilityFrom(classDeclaration.Modifiers),
 				BaseType = typeSymbol.BaseType.Name,
 				Interfaces = typeSymbol.Interfaces.Select(i => i.Name),
-				Fields = _getFields.Execute(typeSymbol)
+				Fields = _getFields.Execute(typeSymbol),
+				Constructors = _getConstructors.Execute(typeSymbol)
 			};
+		}
+	}
+
+	public class GetAllCtorsQuery
+	{
+		public IEnumerable<CtorDetails> Execute(ITypeSymbol type)
+		{
+			throw new NotImplementedException();
 		}
 	}
 }

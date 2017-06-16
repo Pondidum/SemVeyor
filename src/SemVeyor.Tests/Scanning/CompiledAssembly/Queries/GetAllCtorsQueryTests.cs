fad5ddb2e01b36eed24199a521c444f35bc25952
andy <andy@andydote.co.uk>
2017-06-16 22:57:47 +0300

    getAllCtorsQuery: centralise test cases

--- src/SemVeyor.Tests/Scanning/CompiledAssembly/Queries/GetAllCtorsQueryTests.cs
+++ src/SemVeyor.Tests/Scanning/CompiledAssembly/Queries/GetAllCtorsQueryTests.cs
@@ -1,43 +1,15 @@
 ﻿using System.Collections.Generic;
-using System.Linq;
 using SemVeyor.Domain;
 using SemVeyor.Scanning.CompiledAssembly.Queries;
-using SemVeyor.Tests.Domain;
 using SemVeyor.Tests.TestUtils;
-using Shouldly;
-using Xunit;
 
 namespace SemVeyor.Tests.Scanning.CompiledAssembly.Queries
 {
-	public class GetAllCtorsQueryTests
+	public class GetAllCtorsQueryTests : GetAllCtorsQueryTestBase
 	{
-		private readonly IEnumerable<CtorDetails> _constructors;
-
-		public GetAllCtorsQueryTests()
+		protected override IEnumerable<CtorDetails> BuildDetails()
 		{
-			_constructors = new GetAllCtorsQuery().Execute(typeof(TestType));
+			return new GetAllCtorsQuery().Execute(typeof(TestType));
 		}
-
-		[Fact]
-		public void There_are_2_constructors() => _constructors.Count().ShouldBe(2);
-
-		[Fact]
-		public void The_public_ctor_is_listed() => _constructors.ShouldContain(x => x.Visibility == Visibility.Public);
-
-		[Fact]
-		public void The_protected_ctor_is_listed() => _constructors.ShouldContain(x => x.Visibility == Visibility.Protected);
-
-		[Fact]
-		public void The_internal_ctor_is_not_listed() => _constructors.ShouldNotContain(x => x.Visibility == Visibility.Internal);
-
-		[Fact]
-		public void The_private_ctor_is_not_listed() => _constructors.ShouldNotContain(x => x.Visibility == Visibility.Private);
-
-		[Fact]
-		public void The_ctor_name_is_populated() => _constructors.ByVisibility(Visibility.Protected).Name.ShouldBe(".ctor");
-
-		[Fact]
-		public void The_ctor_arguments_are_populated() => _constructors.ByVisibility(Visibility.Protected).Parameters.Count().ShouldBe(1);
-
 	}
 }

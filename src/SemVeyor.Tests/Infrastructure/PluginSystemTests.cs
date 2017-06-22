1344fa93015113ec4e3bb42a80db28a7efd8e8de
andy <andy@andydote.co.uk>
2017-06-22 23:20:51 +0300

    pluginSystem: implement building of parameterless and options parameter types

--- src/SemVeyor.Tests/Infrastructure/PluginSystemTests.cs
+++ src/SemVeyor.Tests/Infrastructure/PluginSystemTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using SemVeyor.CommandLine;
 using SemVeyor.Infrastructure;
 using SemVeyor.Storage;
 using Shouldly;
@@ -22,6 +23,60 @@ namespace SemVeyor.Tests.Infrastructure
 				.DiscoverImplementations<IStorage>()
 				.ShouldHaveSingleItem()
 				.ShouldBe(typeof(FileStore));
+		}
+
+		[Fact]
+		public void When_building_a_type_not_castable()
+		{
+			Should.Throw<ArgumentException>(() => _plugins.Build<IStorage>(typeof(Guid), null));
+		}
+
+		[Fact]
+		public void When_building_a_type_with_no_visible_constructors()
+		{
+			Should.Throw<MissingMethodException>(() => _plugins.Build<NoVisibleCtor>(typeof(NoVisibleCtor), null));
+		}
+
+		[Fact]
+		public void When_building_a_type_with_a_parameterless_constructor()
+		{
+			_plugins
+				.Build<ParameterlessCtor>(typeof(ParameterlessCtor), null)
+				.ShouldBeOfType<ParameterlessCtor>();
+		}
+
+		[Fact]
+		public void When_building_a_type_with_an_options_parameter()
+		{
+			var options = new Options();
+
+			_plugins
+				.Build<OptionsParameter>(typeof(OptionsParameter), options)
+				.Options
+				.ShouldBe(options);
+		}
+
+		[Fact]
+		public void When_building_a_type_with_a_poco()
+		{
+		}
+
+		public class NoVisibleCtor
+		{
+			private NoVisibleCtor() {}
+		}
+
+		public class ParameterlessCtor
+		{
+		}
+
+		public class OptionsParameter
+		{
+			public Options Options { get; }
+			public OptionsParameter(Options options)
+			{
+				Options = options;
+			}
 		}
 	}
 }
--- src/SemVeyor/Infrastructure/PluginSystem.cs
+++ src/SemVeyor/Infrastructure/PluginSystem.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using SemVeyor.CommandLine;
 
 namespace SemVeyor.Infrastructure
 {
@@ -14,6 +15,30 @@ namespace SemVeyor.Infrastructure
 				.GetExportedTypes()
 				.Where(t => t.IsAbstract == false && t.IsClass)
 				.Where(t => t.GetInterfaces().Contains(typeof(T)));
+		}
+
+		public T Build<T>(Type type, Options options)
+		{
+			if (typeof(T).IsAssignableFrom(type) == false)
+				throw new ArgumentException($"{type.FullName} cannot be cast to {typeof(T).FullName}", nameof(type));
+
+			var constructors = type.GetConstructors();
+
+			if (constructors.Any() == false)
+				throw new MissingMethodException(type.FullName, "ctor");
+
+			var blank = constructors.FirstOrDefault(c => c.GetParameters().Length == 0);
+
+			if (blank != null)
+				return (T)blank.Invoke(new object[0] { });
+
+			var optionsOnly = constructors
+				.FirstOrDefault(c => c.GetParameters().Length == 1 && c.GetParameters().Single().ParameterType == typeof(Options));
+
+			if (optionsOnly != null)
+				return (T)optionsOnly.Invoke(new object[] { options });
+
+			throw new NotImplementedException();
 		}
 	}
 }

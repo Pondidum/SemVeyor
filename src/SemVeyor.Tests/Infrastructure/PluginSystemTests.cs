99c97f6213488703bba1fe7ad026b312797d8fd7
andy <andy@andydote.co.uk>
2017-06-22 22:41:07 +0300

    infra: implement type discovery

---
+++ src/SemVeyor.Tests/Infrastructure/PluginSystemTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using SemVeyor.Infrastructure;
+using SemVeyor.Storage;
+using Shouldly;
+using Xunit;
+
+namespace SemVeyor.Tests.Infrastructure
+{
+	public class PluginSystemTests
+	{
+		private readonly PluginSystem _plugins;
+
+		public PluginSystemTests()
+		{
+			_plugins = new PluginSystem();
+		}
+
+		[Fact]
+		public void When_discovering_types()
+		{
+			_plugins
+				.DiscoverImplementations<IStorage>()
+				.ShouldHaveSingleItem()
+				.ShouldBe(typeof(FileStore));
+		}
+	}
+}
---
+++ src/SemVeyor/Infrastructure/PluginSystem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SemVeyor.Infrastructure
+{
+	public class PluginSystem
+	{
+		public IEnumerable<Type> DiscoverImplementations<T>()
+		{
+			var assembly = GetType().Assembly;
+
+			return assembly
+				.GetExportedTypes()
+				.Where(t => t.IsAbstract == false && t.IsClass)
+				.Where(t => t.GetInterfaces().Contains(typeof(T)));
+		}
+	}
+}

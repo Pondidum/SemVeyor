3d4132e869c31b71de5a5bf7d80590ab0251252e
andy <andy@andydote.co.uk>
2017-07-05 23:50:50 +0300

    files: fix indentation

--- src/SemVeyor.Tests/Configuration/ConfigFileReaderTests.cs
+++ src/SemVeyor.Tests/Configuration/ConfigFileReaderTests.cs
@@ -9,94 +9,95 @@ using Xunit;
 
 namespace SemVeyor.Tests.Configuration
 {
-    public class ConfigFileReaderTests
-    {
-        private readonly IFileSystem _filesystem;
-        private readonly ConfigFileReader _reader;
-        private readonly string _path;
 
-        public ConfigFileReaderTests()
-        {
-            _filesystem = Substitute.For<IFileSystem>();
-            _reader = new ConfigFileReader(_filesystem);
-            _path = ConfigFileReader.FilePaths.First();
-        }
+	public class ConfigFileReaderTests
+	{
+		private readonly IFileSystem _filesystem;
+		private readonly ConfigFileReader _reader;
+		private readonly string _path;
 
-        private void FileContents(string text)
-        {
-            var contents = new MemoryStream(Encoding.UTF8.GetBytes(text));
+		public ConfigFileReaderTests()
+		{
+			_filesystem = Substitute.For<IFileSystem>();
+			_reader = new ConfigFileReader(_filesystem);
+			_path = ConfigFileReader.FilePaths.First();
+		}
 
-            _filesystem.FileExists(_path).Returns(true);
-            _filesystem.ReadFile(_path).Returns(contents);
-        }
+		private void FileContents(string text)
+		{
+			var contents = new MemoryStream(Encoding.UTF8.GetBytes(text));
 
-        private void ShouldBeDefault(Config config)
-        {
-            config.ShouldSatisfyAllConditions(
-                () => config.GlobalOptions.Paths.ShouldBeEmpty(),
-                () => config.GlobalOptions.ReadOnly.ShouldBeFalse(),
-                () => config.StorageTypes.ShouldBe(new[] {Options.DefaultStorage}),
-                () => config.ReporterTypes.ShouldBe(new[] {Options.DefaultReporter})
-            );
-        }
+			_filesystem.FileExists(_path).Returns(true);
+			_filesystem.ReadFile(_path).Returns(contents);
+		}
 
-        [Fact]
-        public void When_a_config_file_does_not_exist()
-        {
-            _filesystem.FileExists(_path).Returns(false);
+		private void ShouldBeDefault(Config config)
+		{
+			config.ShouldSatisfyAllConditions(
+				() => config.GlobalOptions.Paths.ShouldBeEmpty(),
+				() => config.GlobalOptions.ReadOnly.ShouldBeFalse(),
+				() => config.StorageTypes.ShouldBe(new[] {Options.DefaultStorage}),
+				() => config.ReporterTypes.ShouldBe(new[] {Options.DefaultReporter})
+			);
+		}
 
-            ShouldBeDefault(_reader.Read());
-        }
+		[Fact]
+		public void When_a_config_file_does_not_exist()
+		{
+			_filesystem.FileExists(_path).Returns(false);
 
-        [Fact]
-        public void When_a_config_file_is_blank()
-        {
-            FileContents("");
-            ShouldBeDefault(_reader.Read());
-        }
+			ShouldBeDefault(_reader.Read());
+		}
 
-        [Fact]
-        public void When_a_config_hasnt_got_any_keys()
-        {
-            FileContents("{}");
-            ShouldBeDefault(_reader.Read());
-        }
+		[Fact]
+		public void When_a_config_file_is_blank()
+		{
+			FileContents("");
+			ShouldBeDefault(_reader.Read());
+		}
 
-        [Fact]
-        public void When_a_config_has_properties()
-        {
-            FileContents(@"
+		[Fact]
+		public void When_a_config_hasnt_got_any_keys()
+		{
+			FileContents("{}");
+			ShouldBeDefault(_reader.Read());
+		}
+
+		[Fact]
+		public void When_a_config_has_properties()
+		{
+			FileContents(@"
 {
-    ""readonly"": true,
-    ""paths"": [ ""some\\file\\path.dll"" ]
+	""readonly"": true,
+	""paths"": [ ""some\\file\\path.dll"" ]
 }");
-            var config = _reader.Read();
+			var config = _reader.Read();
 
-            config.ShouldSatisfyAllConditions(
-                () => config.GlobalOptions.ReadOnly.ShouldBeTrue(),
-                () => config.GlobalOptions.Paths.ShouldBe(new[] {"some\\file\\path.dll"}),
-                () => config.StorageTypes.ShouldBe(new[] {Options.DefaultStorage})
-            );
-        }
+			config.ShouldSatisfyAllConditions(
+				() => config.GlobalOptions.ReadOnly.ShouldBeTrue(),
+				() => config.GlobalOptions.Paths.ShouldBe(new[] {"some\\file\\path.dll"}),
+				() => config.StorageTypes.ShouldBe(new[] {Options.DefaultStorage})
+			);
+		}
 
-        [Fact]
-        public void When_a_config_file_has_a_storage_node_with_a_key()
-        {
-            FileContents(@"
+		[Fact]
+		public void When_a_config_file_has_a_storage_node_with_a_key()
+		{
+			FileContents(@"
 {
-    ""readonly"": true,
-    ""paths"": [ ""some\\file\\path.dll"" ],
-    ""storage"": {
-        ""aws:s3"": { ""accesskey"": ""1234"", ""secretkey"": ""wat"" }
-    }
+	""readonly"": true,
+	""paths"": [ ""some\\file\\path.dll"" ],
+	""storage"": {
+		""aws:s3"": { ""accesskey"": ""1234"", ""secretkey"": ""wat"" }
+	}
 }");
-            var config = _reader.Read();
+			var config = _reader.Read();
 
-            config.ShouldSatisfyAllConditions(
-                () => config.GlobalOptions.ReadOnly.ShouldBeTrue(),
-                () => config.GlobalOptions.Paths.ShouldBe(new[] {"some\\file\\path.dll"}),
-                () => config.StorageTypes.ShouldBe(new[] {"aws:s3"})
-            );
-        }
-    }
+			config.ShouldSatisfyAllConditions(
+				() => config.GlobalOptions.ReadOnly.ShouldBeTrue(),
+				() => config.GlobalOptions.Paths.ShouldBe(new[] {"some\\file\\path.dll"}),
+				() => config.StorageTypes.ShouldBe(new[] {"aws:s3"})
+			);
+		}
+	}
 }

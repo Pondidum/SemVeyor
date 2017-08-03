8ec23973879897d4119b2d93c3ad2330430f1977
andy <andy@andydote.co.uk>
2017-08-03 23:11:19 +0300

    configurationBuilder: refactor build method

--- src/SemVeyor/Configuration/ConfigurationBuilder.cs
+++ src/SemVeyor/Configuration/ConfigurationBuilder.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.IO;
+﻿using System.IO;
 using System.Linq;
 using FileSystem;
 using SemVeyor.Commands.Scan;
@@ -9,7 +8,7 @@ namespace SemVeyor.Configuration
 	public class ConfigurationBuilder
 	{
 		private static readonly string DefaultConfigFile = ConfigFileReader.FilePaths.First();
-		
+
 		private readonly IFileSystem _fileSystem;
 		private readonly ConfigFileReader _configReader;
 
@@ -19,40 +18,26 @@ namespace SemVeyor.Configuration
 			_configReader = new ConfigFileReader(_fileSystem);
 		}
 
+		private static bool CustomConfigSpecified(ScanInput input) => string.IsNullOrWhiteSpace(input.ConfigFlag) == false;
+		private static bool TargetSpecified(ScanInput input) => string.IsNullOrWhiteSpace(input.Path) == false;
+
 		public Config Build(ScanInput commandLine)
 		{
-			if (string.IsNullOrWhiteSpace(commandLine.ConfigFlag) == false)
-			{
-				if (_fileSystem.FileExists(commandLine.ConfigFlag).Result)
-				{
-					return _configReader.Read(commandLine.ConfigFlag).OverrideWith(commandLine.AsOptions());
-				}
-				else
-				{
-					throw new FileNotFoundException("Could not find config file.", commandLine.ConfigFlag);
-				}
-			}
+			if (CustomConfigSpecified(commandLine))
+				return ReadFile(commandLine.ConfigFlag, commandLine.AsOptions());
 
-			if (string.IsNullOrWhiteSpace(commandLine.ConfigFlag))
-			{
-				if (_fileSystem.FileExists(DefaultConfigFile).Result == false)
-				{
-					if (string.IsNullOrWhiteSpace(commandLine.Path) == false)
-					{
-						return new Config().OverrideWith(commandLine.AsOptions());
-					}
-					else
-					{
-						throw new FileNotFoundException("Could not find config file.", commandLine.ConfigFlag);
-					}
-				}
-				else
-				{
-					return _configReader.Read(DefaultConfigFile).OverrideWith(commandLine.AsOptions());
-				}
-			}
-			
-			throw new NotSupportedException();
+			if (TargetSpecified(commandLine))
+				return new Config().OverrideWith(commandLine.AsOptions());
+
+			return ReadFile(DefaultConfigFile, commandLine.AsOptions());
+		}
+
+		private Config ReadFile(string path, Options options)
+		{
+			if (_fileSystem.FileExists(path).Result)
+				return _configReader.Read(path).OverrideWith(options);
+
+			throw new FileNotFoundException("Could not find config file.", path);
 		}
 	}
 }

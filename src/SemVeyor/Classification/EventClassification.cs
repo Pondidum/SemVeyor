79982ede8f3045177dc78abd975c17ecbee9b212
andy <andy@andydote.co.uk>
2017-08-06 19:59:21 +0300

    eventClassification: refactor to use map of versions rather than interfaces

--- src/SemVeyor/Classification/EventClassification.cs
+++ src/SemVeyor/Classification/EventClassification.cs
@@ -13,15 +13,11 @@ namespace SemVeyor.Classification
 		public SemVer ClassifyEvent(object @event)
 		{
 			var eventType = @event as Type ?? @event.GetType();
-			var interfaces = eventType.GetInterfaces();
 
-			if (interfaces.Contains(Major))
-				return SemVer.Major;
-
-			if (interfaces.Contains(Minor))
-				return SemVer.Minor;
-
-			return SemVer.None;
+			SemVer version;
+			return VersionMap.TryGetValue(eventType.Name, out version)
+				? version
+				: SemVer.None;
 		}
 
 		public IEnumerable<ChangeClassification> ClassifyAll(IEnumerable<object> events)
@@ -32,5 +28,57 @@ namespace SemVeyor.Classification
 				Classification = ClassifyEvent(@event)
 			});
 		}
+
+		private static readonly Dictionary<string, SemVer> VersionMap = new Dictionary<string, SemVer>
+		{
+			{ nameof(AssemblyTypeAdded), SemVer.Minor },
+			{ nameof(AssemblyTypeRemoved), SemVer.Major },
+
+			{ nameof(TypeVisibilityIncreased), SemVer.Minor },
+			{ nameof(TypeVisibilityDecreased), SemVer.Major },
+			{ nameof(TypeFieldAdded), SemVer.Minor },
+			{ nameof(TypeFieldRemoved), SemVer.Major },
+			{ nameof(TypeMethodAdded), SemVer.Minor },
+			{ nameof(TypeMethodRemoved), SemVer.Major },
+			{ nameof(TypePropertyAdded), SemVer.Minor },
+			{ nameof(TypePropertyRemoved), SemVer.Major },
+			{ nameof(TypeCtorAdded), SemVer.Minor },
+			{ nameof(TypeCtorRemoved), SemVer.Major },
+			{ nameof(TypeGenericArgumentAdded), SemVer.Minor },
+			{ nameof(TypeGenericArgumentRemoved), SemVer.Major },
+
+			{ nameof(FieldVisibilityIncreased), SemVer.Minor },
+			{ nameof(FieldVisibilityDecreased), SemVer.Major },
+			{ nameof(FieldTypeChanged), SemVer.Major },
+
+			{ nameof(GenericArgumentPositionChanged), SemVer.Major },
+			{ nameof(GenericArgumentNameChanged), SemVer.Major },
+			{ nameof(GenericArgumentConstraintAdded), SemVer.Minor },
+			{ nameof(GenericArgumentConstraintRemoved), SemVer.Major },
+
+			{ nameof(ParameterNameChanged), SemVer.Major },
+			{ nameof(ParameterTypeChanged), SemVer.Major },
+			{ nameof(ParameterMoved), SemVer.Major },
+
+			{ nameof(MethodVisibilityIncreased), SemVer.Minor },
+			{ nameof(MethodVisibilityDecreased), SemVer.Major },
+			{ nameof(MethodNameChanged), SemVer.Major },
+			{ nameof(MethodTypeChanged), SemVer.Major },
+			{ nameof(MethodArgumentAdded), SemVer.Minor },
+			{ nameof(MethodArgumentRemoved), SemVer.Major },
+			{ nameof(MethodGenericArgumentAdded), SemVer.Minor },
+			{ nameof(MethodGenericArgumentRemoved), SemVer.Major },
+
+			{ nameof(PropertyVisibilityDecreased), SemVer.Major },
+			{ nameof(PropertyVisibilityIncreased), SemVer.Minor },
+			{ nameof(PropertyTypeChanged), SemVer.Major },
+			{ nameof(PropertyArgumentAdded), SemVer.Minor },
+			{ nameof(PropertyArgumentRemoved), SemVer.Major },
+
+			{ nameof(CtorVisibilityDecreased), SemVer.Major },
+			{ nameof(CtorVisibilityIncreased), SemVer.Minor },
+			{ nameof(CtorArgumentAdded), SemVer.Minor },
+			{ nameof(CtorArgumentRemoved), SemVer.Major },
+		};
 	}
 }
